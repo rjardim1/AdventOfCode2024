034758656a7a89359b0906fc2182c82c35950cfb
rjardim1 <ruben.luis.neto@gmail.com>
2024-12-06 11:07:13 +0000

    Day 3
    Completed TestData for Day 3

--- Day3/Day3.cpp
+++ Day3/Day3.cpp
@@ -8,33 +8,38 @@ using namespace std;
 
 int main()
 {
-    string line;
-    ifstream file("D:\\AdventOfCode2024\\Day3\\TestData.txt");
     int result = 0;
+    string file_name(R"(E:\AdventOfCode2024\Day3\TestData.txt)");
+    ifstream source_file(file_name);
+    stringstream source_file_ss;
+    source_file_ss << source_file.rdbuf();
+    const string source_file_str = source_file_ss.str();
 
-    while (getline(file, line))
+    std::regex mul_regex(R"(mul\(([0-9]+),([0-9]+)\))");
+    auto words_end = std::sregex_iterator();
+    auto words_begin = std::sregex_iterator(source_file_str.begin(), source_file_str.end(), mul_regex);
+
+    for (std::sregex_iterator word = words_begin; word != words_end; ++word)
     {
-        std::regex mul_regex(R"(mul\(([0-9]+),([0-9]+)\))");
-        auto words_end = std::sregex_iterator();
-        auto words_begin = std::sregex_iterator(line.begin(), line.end(), mul_regex);
+        string str_in_file = word->str();
+        std::smatch match;
 
-        for (std::sregex_iterator i = words_begin; i != words_end; ++i)
+        if (std::regex_search(str_in_file, match, mul_regex))
         {
-            std::smatch match = *i;
-            std::string match_str = match.str();
-            cout << match_str << endl;
-            
-            stringstream line_stream(line);
-            const char seperator = ',';
-            string number = "";
-            vector<int> list;
+            vector<int> regex_matches;
 
-            while (getline(line_stream, number, seperator))
+            for (const auto& matched_data : match)
             {
-                list.push_back(stoi(number));
-            }
-        }
+                string data = matched_data.str();
 
+                if (!data.empty() && std::all_of(data.begin(), data.end(), ::isdigit))
+                {
+                    regex_matches.push_back(stoi(data));
+                }
+            }
+
+            result += regex_matches.at(0) * regex_matches.at(1);
+        }
     }
 
     cout << result << endl;
